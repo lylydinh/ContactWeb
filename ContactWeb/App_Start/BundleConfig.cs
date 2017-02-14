ed19afdb291262370e717639fbe20dc987cd859d
Bùi Đình Thuy <buithuydinh.epu@gmail.com>
2017-02-14 22:42:31 +0700

    Tao moi Project
    Khoi tao va chia thanh cac muc khac nhau

---
+++ ContactWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Optimization;
+
+namespace ContactWeb
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                         "~/Content/js/lib/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Content/js/lib/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/Content/js/lib/jquery.dataTables.min.js",
+                        "~/Content/js/lib/dataTables.bootstrap.min.js",
+                        "~/Content/js/lib/dataTables.colReorder.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/css/jquery.dataTables.min.css",
+                      "~/Content/css/jquery.dataTables_themeroller.css",
+                      "~/Content/css/dataTables.bootstrap.min.css",
+                      "~/Content/css/colReorder.bootstrap.min.css",
+                      "~/Content/css/site.css"));
+        }
+    }
+}
